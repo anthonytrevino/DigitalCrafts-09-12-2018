38b7a398e8ac95b115440880ba0d62a1a8d85c3c
anthonytrevino <43328074+anthonytrevino@users.noreply.github.com>
2018-09-19 15:54:05 -0500

    Add files via upload
    Initial commit

---
+++ Factorial.py
@@ -0,0 +1,6 @@
+
+user_input = int(input("Enter a number: "))
+
+import math
+result = math.factorial(user_input)
+print(result)
---
+++ Palindrome.py
@@ -0,0 +1,9 @@
+n = input("Enter a word: ")
+
+if str(n) == str(n)[::-1]:
+   print("palindrome")
+else:
+   print("Not a palindrome.")
+
+
+print(n)
